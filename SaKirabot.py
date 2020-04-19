cf18a2f7ba6ebbab0f5600632aa1e7e26fc5ed36
CezSiex <63515454+CezSiex@users.noreply.github.com>
2020-04-19 14:14:06 +0300

    Update SaKirabot.py

--- SaKirabot.py
+++ SaKirabot.py
@@ -1,2 +1,13 @@
 import telebot
 import config22
+
+bot = telebot.TeleBot(config22.TOKEN) 
+
+# Start
+@bot.message_handler(commands=['Hey'])
+def send_start(message):
+	bot.send_message(message.chat.id, '( >_<)')
+
+  
+#RUN
+bot.polling(none_stop=True)
